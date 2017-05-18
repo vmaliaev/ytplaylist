efa802bfc2a9cedac9cfd5c173121c9849788cf1
Vladimir Maliaev <vm321d@att.com>
2017-05-18 00:22:42 -0500

    v1

--- dc.py
+++ dc.py
@@ -1,18 +1,12 @@
 #!/Users/vmaliaev/venv1/bin/python
 
 ##!/usr/bin/python
-import sys
 import os
+import sys
 import errno
+
 import pafy
 from pydub import AudioSegment
-
-# plurl = "https://www.youtube.com/playlist?list=PLC38Xa1H2ja2qU1LSzOHMfI7MFMkXHIjq"
-# >>> playlist = pafy.get_playlist(plurl)
-# >>> for i in playlist['items']: i['pafy'].getbestaudio().download()
-
-# >>> from pydub import AudioSegment
-# >>> AudioSegment.from_file("Life Begins at the End of Your Comfort Zone | Yubing Zhang | TEDxStanford.opus").export("test-11.mp3",format="mp3")
 
 
 def get_title(arg):
@@ -22,20 +16,40 @@ def stru(arg):
 #    print arg
     for i in arg:
         print i
-        playlist = get_title(i)
+        try:
+            playlist = get_title(i)
+        except ValueError as e:
+            print "\nERROR: Check url: "+i
+            print e
+            print "Continue next playlist\n"
+            break
         title = playlist["title"]
-        print "Title=",title
+        newpath_video = "Playlist="+title
+        newpath_audio = newpath_video+"/audio"
+        print "Playlist:",title
         try:
-            os.makedirs("Playlist="+title)
+            os.makedirs(newpath_video)
         except OSError as exception:
             if exception.errno != errno.EEXIST:
                 raise
         try:
-            os.makedirs("Playlist="+title+"/audio")
+            os.makedirs(newpath_audio)
         except OSError as exception:
             if exception.errno != errno.EEXIST:
                 raise
-#        for k in playlist['items']: k['pafy'].getbestaudio().download()
+        for k in playlist['items']:
+            print k['pafy'].title
+            if os.path.isfile(newpath_audio+"/"+k['pafy'].title+"."+k['pafy'].getbestaudio().extension): print "File already exists" ;continue
+            k['pafy'].getbest().download(filepath=newpath_video)
+            k['pafy'].getbestaudio().download(filepath=newpath_audio)
+            break
+            
+        for (dirp, dirn, f) in os.walk(newpath_audio):
+            break
+        for fname in f:
+            fext = fname.split(".")[-1]
+            if fext == "mp3" and fext == "temp" : continue #print fname+"!."+fext
+            AudioSegment.from_file(newpath_audio+"/"+fname).export(newpath_audio+"/"+fname[:-len(fext)]+"mp3" , format="mp3")
 
 if __name__ == "__main__":
 
